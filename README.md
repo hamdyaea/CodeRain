6ad7bbe1a63dc9c0d974a2a6584d8797f2a32be0
hamdyaea <hamdy.aea@gmail.com>
2019-03-13 10:49:13 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# CodeRain
+
+An infinite loop that print in the linux terminal you name or a message with random ascii letters like in the Matrix
+
+## What you need :
+
+Python3 and linux
+
+## Developer - Author 
+
+Hamdy Abou El Anein
+
+## Homepage 
+
+http://www.daylightlinux.ch
+
+
---
+++ coderain.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+# Developer : Hamdy Abou El Anein
+
+# An infinite loop print your name and random characters like in the Matrix
+
+import random
+import string
+
+def random_char():
+    global characters
+    characters = ''.join(random.choice(string.ascii_letters) for x in range(random.randint(1,50)))
+
+
+def start():
+    global characters
+
+    class bcolors:
+        OKGREEN = '\033[92m'
+        ENDC = '\033[0m'
+
+
+    reply = input("Enter your name here : ")
+
+
+    while True:
+        random_char()
+        #for i in range(100):
+
+        print(bcolors.OKGREEN + (characters)+((reply).center(random.randint(1,250)))+(characters)+ bcolors.ENDC)
+
+start()
