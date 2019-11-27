41f68a57dde22871e3ac1ea6e3eb28e89508c591
hamdyaea <hamdy.aea@protonmail.com>
2019-11-27 20:40:53 +0100

    Code beauty and class

--- coderain.py
+++ coderain.py
@@ -1,36 +1,46 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 # Developer : Hamdy Abou El Anein
+# hamdy.aea@protonmail.com
 
 # An infinite loop print your name and random characters like in the Matrix
 
 import random
 import string
 
+
+class Matrix:
+    def __init__(self):
+        self.characters
+
+
 def random_char():
-    global characters
-    characters = ''.join(random.choice(string.ascii_letters) for x in range(random.randint(1,50)))
+    Matrix.characters = "".join(
+        random.choice(string.ascii_letters) for x in range(random.randint(1, 50))
+    )
 
 
 def start():
-    global characters
-
     class bcolors:
-        OKGREEN = '\033[92m'
-        ENDC = '\033[0m'
-
+        OKGREEN = "\033[92m"
+        ENDC = "\033[0m"
 
     reply = input("Enter your name here : ")
-
 
     while True:
         try:
             random_char()
 
+            print(
+                bcolors.OKGREEN
+                + (Matrix.characters)
+                + ((reply).center(random.randint(1, 250)))
+                + (Matrix.characters)
+                + bcolors.ENDC
+            )
 
-            print(bcolors.OKGREEN + (characters)+((reply).center(random.randint(1,250)))+(characters)+ bcolors.ENDC)
-
-        except KeyboardInterrupt:     # Ctrl +c to stop the code rain
+        except KeyboardInterrupt:  # Ctrl +c to stop the code rain
             break
+
 
 start()
